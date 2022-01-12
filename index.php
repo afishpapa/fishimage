dbe8bf1ee3d9b918d3b550c31eec4b37ea8d4806
sonj <116265858@qq.com>
2022-01-12 20:54:42 +0800

    xx

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 
 
 require_once('getID/getid3/getid3.php');
-set_time_limit(30);
+set_time_limit(0);
 
 // Initialize getID3 engine
 $getID3 = new getID3;
@@ -32,14 +32,23 @@ while (($file = readdir($dir)) !== false) {
 	$oldName = $DirectoryToScan.'/'.$file;
 	if ($fileType == 'mp4') {
 		$ThisFileInfo = $getID3->analyze($FullFileName);
+
 		$getID3->CopyTagsToComments($ThisFileInfo);
-		$date =  date('YmdHis', $ThisFileInfo['quicktime']['moov']['subatoms'][0]['creation_time_unix']);
-		$newFileName = $DirectoryToScan . '/video/' . $date . '.'. pathinfo($file, PATHINFO_EXTENSION);	
+		if ($ThisFileInfo['quicktime']['moov']['subatoms'][0]['creation_time_unix'] < 1600000000) {
+            $date =  date('YmdHis', $ThisFileInfo['quicktime']['moov']['subatoms'][0]['modify_time_unix']);
+        } else {
+            $date =  date('YmdHis', $ThisFileInfo['quicktime']['moov']['subatoms'][0]['creation_time_unix']);
+        }
+        echo json_encode($ThisFileInfo['quicktime']['moov']['subatoms'][0]).PHP_EOL;
+        echo $date;;
+        $newFileName = $DirectoryToScan . '/video/' . $date . '.'. pathinfo($file, PATHINFO_EXTENSION);
 	} 
 	if ($fileType == 'jpg' || $fileType == 'png') {
-		$img = exif_read_data($FullFileName);		
-		$date =  date('YmdHis', $img['FileDateTime']);
-		$newFileName = $DirectoryToScan . '/image/' . $date . '.'. pathinfo($file, PATHINFO_EXTENSION);	
+//		$img = exif_read_data($FullFileName);
+//		$date =  date('YmdHis', $img['FileDateTime']);
+//		$newFileName = $DirectoryToScan . '/image/' . $date . '.'. pathinfo($file, PATHINFO_EXTENSION);
+        $date = mb_substr($file, 5);
+        $newFileName = $DirectoryToScan . '/image/' . $date;
 	}
 	copy($oldName, $newFileName);
 }
